91cb560930423e6025c3e8c04e4bcac477646613
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-10-02 23:33:23 +0200

    Create lab package

---
+++ packages/janribkaui-lab/README.md
@@ -0,0 +1,22 @@
+# @janribkaui/lab
+
+This package hosts the incubator components that are not yet ready to move to `core`.
+
+## Installation
+
+Install the package in your project directory with:
+
+<!-- #default-branch-switch -->
+
+```bash
+npm install @janribkaui/lab@next
+```
+
+The lab has peer dependencies on the Material Design components.
+If you are not already using them in your project, you can install with:
+
+<!-- #default-branch-switch -->
+
+```bash
+npm install @janribkaui/material-ui-tailwind@next
+```
---
+++ packages/janribkaui-lab/tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'tailwindcss';
+
+import basePlugin from '@janribka/ui/styles/basePlugin';
+import colorsPlugin from '@janribka/ui/styles/colorsPlugin';
+import transitionsPlugin from '@janribka/ui/styles/transitionsPlugin';
+
+// TODO: Dát zde výchozí font roboto podle mui
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  // mode: 'jit',
+  theme: {
+    extend: {
+      transitionProperty: {
+        'background-color': 'background-color',
+        'box-shadow': 'box-shadow',
+        'border-color': 'border-color',
+        color: 'color',
+        button: 'background-color, color, box-shadow, border-color',
+        'stroke-dashoffset': 'stroke-dashoffset',
+      },
+      lineHeight: {
+        '4_67': '1.167',
+        '4_8': '1.2',
+        '4_94': '1.235',
+        '5_34': '1.334',
+        '6_4': '1.6',
+      },
+      opacity: {
+        hover: '0.04',
+        selected: '0.08',
+        disabled: '0.38',
+        focus: '0.12',
+        activated: '0.12',
+        hoverDark: '0.08',
+        selectedDark: '0.16',
+        disabledDark: '0.38',
+        focusDark: '0.12',
+        activatedDark: '0.24',
+      },
+      flex: {
+        noneAuto: '0 0 auto',
+      },
+    },
+  },
+  plugins: [basePlugin({}), colorsPlugin({}), transitionsPlugin({})],
+};
+
+export default config;
